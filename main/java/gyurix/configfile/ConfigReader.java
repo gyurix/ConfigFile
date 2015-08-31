b1431cf8391385e5c776dacabb34fda05dfb5702
gyurix <barathgy@gmail.com>
2015-09-01 00:18:16 +0200

    Revert "Revert "Temporary file on saving config, bug fixes""
    This reverts commit 0d32359a9e626ea80653fa95abae15fa790f5e59.

--- main/java/gyurix/configfile/ConfigReader.java
+++ main/java/gyurix/configfile/ConfigReader.java
@@ -4,105 +4,103 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 
+public class ConfigReader {
+    final int blockLvl;
+    final ConfigData value;
+    final ConfigData key;
+    private boolean keyRead = false;
 
+    ConfigReader(int lvl, ConfigData data) {
+        this.blockLvl = lvl;
+        this.value = data;
+        this.key = null;
+    }
 
+    ConfigReader(int lvl, ConfigData key, ConfigData value) {
+        this.blockLvl = lvl;
+        this.key = key;
+        this.value = value;
+    }
 
-public class ConfigReader
-{
-  private boolean keyRead = false;
-  final int blockLvl;
-  
-  ConfigReader(int lvl, ConfigData data) {
-    this.blockLvl = lvl;
-    this.value = data;
-    this.key = null;
-  }
-  
-  ConfigReader(int lvl, ConfigData key, ConfigData value) { this.blockLvl = lvl;
-    this.key = key;
-    this.value = value;
-  }
-  
-  public ConfigData getData() { return this.keyRead ? this.key : this.value; }
-  
-  public void addComment(String com) {
-    ConfigData data = getData();
-    if (data.comment == null) {
-      data.comment = com;
-    } else { ConfigData tmp28_27 = data;tmp28_27.comment = (tmp28_27.comment + '\n' + com); } }
-  
-  final ConfigData value;
-  final ConfigData key;
-  public void handleInput(ArrayList<ConfigReader> readers, String line, int lvl) { ConfigData data = getData();
-    if ((line.equals("-")) || (line.equals(">")) || (line.indexOf(":") == line.length() - 1))
-      line = line + ' ';
-    if (line.startsWith("- ")) {
-      if (data.listData == null)
-        data.listData = new ArrayList();
-      ConfigData d = new ConfigData();
-      data.listData.add(d);
-      ConfigReader r = new ConfigReader(lvl + 1, d);
-      readers.add(r);
-      r.handleInput(readers, line.substring(2), lvl + 2);
+    public ConfigData getData() {
+        return this.keyRead ? this.key : this.value;
     }
-    else if (line.startsWith("> ")) {
-      ConfigData key = new ConfigData(ConfigData.unescape(line.substring(2)));
-      ConfigData value = new ConfigData();
-      ConfigReader reader = new ConfigReader(lvl, key, value);
-      reader.keyRead = true;
-      if (lvl == this.blockLvl) {
-        int size = readers.size() - 2;
-        readers.remove(size + 1);
-        ((ConfigReader)readers.get(size)).getData().mapData.put(key, value);
-      }
-      else {
-        if (data.mapData == null)
-          data.mapData = new LinkedHashMap();
-        data.mapData.put(key, value);
-      }
-      readers.add(reader);
-    }
-    else if ((this.keyRead) && (line.startsWith(": "))) {
-      this.keyRead = false;
-      this.value.stringData = ConfigData.unescape(line.substring(2));
-    }
-    else {
-      String[] s = line.startsWith(": ") ? new String[] { line } : line.split(" *: +", 2);
-      if (s.length == 2) {
-          ConfigData key;
-        if (this.keyRead) {
-          this.key.stringData += ConfigData.unescape(new StringBuilder().append("\n").append(s[0]).toString());
-          key = this.key;
-        }
-        else {
-          key = new ConfigData(ConfigData.unescape(s[0]));
-        }
-        ConfigData value = new ConfigData(ConfigData.unescape(s[1]));
-        ConfigReader reader = new ConfigReader(lvl, key, value);
-        if (lvl == this.blockLvl) {
-          int size = readers.size() - 2;
-          readers.remove(size + 1);
-          data = ((ConfigReader)readers.get(size)).getData();
-          if (data.mapData == null) {
-            data.mapData = new LinkedHashMap();
-          }
-          data.mapData.put(key, value);
-        }
-        else {
-          if (data.mapData == null) {
-            data.mapData = new LinkedHashMap();
-          }
-          data.mapData.put(key, value);
-        }
-        readers.add(reader);
 
-      }
-      else if (data.stringData == null) {
-        data.stringData = ConfigData.unescape(line);
-      } else { data.stringData += ConfigData.unescape(new StringBuilder().append("\n").append(line).toString());
-      }
+    public void addComment(String com) {
+        ConfigData data = getData();
+        if (data.comment == null) {
+            data.comment = com;
+        } else {
+            ConfigData tmp28_27 = data;
+            tmp28_27.comment = (tmp28_27.comment + '\n' + com);
+        }
     }
-  }
+
+    public void handleInput(ArrayList<ConfigReader> readers, String line, int lvl) {
+        ConfigData data = getData();
+        if ((line.equals("-")) || (line.equals(">")) || (line.indexOf(":") == line.length() - 1))
+            line = line + ' ';
+        if (line.startsWith("- ")) {
+            if (data.listData == null)
+                data.listData = new ArrayList();
+            ConfigData d = new ConfigData();
+            data.listData.add(d);
+            ConfigReader r = new ConfigReader(lvl + 1, d);
+            readers.add(r);
+            r.handleInput(readers, line.substring(2), lvl + 2);
+        } else if (line.startsWith("> ")) {
+            ConfigData key = new ConfigData(ConfigData.unescape(line.substring(2)));
+            ConfigData value = new ConfigData();
+            ConfigReader reader = new ConfigReader(lvl, key, value);
+            reader.keyRead = true;
+            if (lvl == this.blockLvl) {
+                int size = readers.size() - 2;
+                readers.remove(size + 1);
+                readers.get(size).getData().mapData.put(key, value);
+            } else {
+                if (data.mapData == null)
+                    data.mapData = new LinkedHashMap();
+                data.mapData.put(key, value);
+            }
+            readers.add(reader);
+        } else if ((this.keyRead) && (line.startsWith(": "))) {
+            this.keyRead = false;
+            this.value.stringData = ConfigData.unescape(line.substring(2));
+        } else {
+            String[] s = line.startsWith(": ") ? new String[]{line} : line.split(" *: +", 2);
+            if (s.length == 2) {
+                ConfigData key;
+                if (this.keyRead) {
+                    this.key.stringData += ConfigData.unescape("\n" + s[0]);
+                    key = this.key;
+                } else {
+                    key = new ConfigData(ConfigData.unescape(s[0]));
+                }
+                ConfigData value = new ConfigData(ConfigData.unescape(s[1]));
+                ConfigReader reader = new ConfigReader(lvl, key, value);
+                if (lvl == this.blockLvl) {
+                    int size = readers.size() - 2;
+                    readers.remove(size + 1);
+                    data = readers.get(size).getData();
+                    if (data.mapData == null) {
+                        data.mapData = new LinkedHashMap();
+                    }
+                    data.mapData.put(key, value);
+                } else {
+                    if (data.mapData == null) {
+                        data.mapData = new LinkedHashMap();
+                    }
+                    data.mapData.put(key, value);
+                }
+                readers.add(reader);
+
+            } else if (data.stringData == null) {
+                data.stringData = ConfigData.unescape(line);
+            } else {
+                data.stringData += ConfigData.unescape("\n" + line);
+            }
+        }
+    }
 }
 
 
